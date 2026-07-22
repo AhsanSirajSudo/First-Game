995a2008d94bc30a9dc964f8a264cc48c0fbcc4f
Ahsan Siraj <ahsansiraj00@gmail.com>
2026-07-23 04:22:17 +0500

    adding colour to text

--- Initialize window.py
+++ Initialize window.py
@@ -3,6 +3,7 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Game Window")
 root.geometry("900x600")
+root.configure(background="yellow")
 
 text = tk.Label(root, text = ("Hey monkey"),font = ("Poppins",24))
 text.place(x=64,y=128)
