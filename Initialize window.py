35b19700282f305237f09537158a2e74cdad5d8b
Ahsan Siraj <ahsansiraj00@gmail.com>
2026-07-23 04:38:19 +0500

    added image to window + changed python environment to global

--- Initialize window.py
+++ Initialize window.py
@@ -1,9 +1,18 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 
 root = tk.Tk()
 root.title("Game Window")
 root.geometry("900x600")
 root.configure(background="yellow")
+
+imagefile = r"C:\Users\Ahsan\Downloads\billi.jpg"
+opened_image = Image.open(imagefile)
+img = ImageTk.PhotoImage(opened_image)
+
+label = tk.Label(root, image=img)
+label.pack()
+label.image = img
 
 text = tk.Label(root, text = ("Hey monkey"),font = ("Poppins",24))
 text.place(x=64,y=128)
